1081db635c58f8f87c07f5a1513bba9f8e7d3e15
Laighlinne <mylastmogletter@gmail.com>
2017-12-24 05:46:07 +0900

    Add files via upload

---
+++ FFXIV_ZoomLimitBreaker/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace FFXIV_ZoomLimitBreaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Default = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FFXIV Zoom Limit Breaker";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(187, 130);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(164, 33);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            78,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown2.Location = new System.Drawing.Point(187, 169);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(164, 33);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Field of View";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Zoom Max";
+            // 
+            // Default
+            // 
+            this.Default.Location = new System.Drawing.Point(129, 75);
+            this.Default.Name = "Default";
+            this.Default.Size = new System.Drawing.Size(222, 41);
+            this.Default.TabIndex = 9;
+            this.Default.Text = "Default";
+            this.Default.UseVisualStyleBackColor = true;
+            this.Default.Click += new System.EventHandler(this.Default_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft NeoGothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(126, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Dev: FFXIV KR MOOGLE 라그린네";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Location = new System.Drawing.Point(187, 208);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(164, 33);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 26);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Zoom Min";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::FFXIV_ZoomLimitBreaker.Properties.Resources.lmb;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 256);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Default);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft NeoGothic", 14.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "Form1";
+            this.Text = "FFXIV ZOOM LIMIT BREAKER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Default;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FFXIV_ZoomLimitBreaker/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace FFXIV_ZoomLimitBreaker
+{
+    public partial class Form1 : Form
+    {
+        public string Signiture = "8B****85C074**83F8**75**488B05********83F8**74**4898488D0D********";
+        public int current = 0x128;
+        public int zoommin = 0x12C;
+        public int zoommax = 0x130;
+        public int fofvcur = 0x134;
+        public int fofvmin = 0x138;
+        public int fofvmax = 0x13C;
+        public Form1()
+        {
+            Console.WriteLine("test");
+            Debug.WriteLine("test");
+            InitializeComponent();
+            getValues();
+        }
+
+        private IntPtr m_hProcess;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProcessLoad(0);
+        }
+
+        private void getValues()
+        {
+            numericUpDown1.Value = Convert.ToDecimal(Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "FOV", 0.78f));
+            numericUpDown2.Value = Convert.ToDecimal(Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "ZOOMMAX", 20.0f));
+            numericUpDown3.Value = Convert.ToDecimal(Registry.GetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "ZOOMMIN", 1.5f));
+        }
+
+        private void saveValues()
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "FOV", numericUpDown1.Value);
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "ZOOMMAX", numericUpDown2.Value);
+            Registry.SetValue("HKEY_CURRENT_USER\\SOFTWARE\\ZOOMHACK", "ZOOMMIN", numericUpDown3.Value);
+        }
+
+        private Dictionary<int, IntPtr> sig = new Dictionary<int, IntPtr>();
+
+        private void ProcessLoad(int isFov)
+        {
+            foreach (var i in Process.GetProcesses())
+            {
+                Console.WriteLine(i.ProcessName);
+                if (i.ProcessName == "ffxiv_dx11")
+                {
+                    this.m_hProcess = OpenProcess(ProcessAccessFlags.All, false, i.Id);
+                    var n = IntPtr.Zero;
+
+                    if (!sig.ContainsKey(i.Id))
+                        n = Scan(this.m_hProcess, Signiture, i.MainModule.BaseAddress, i.MainModule.ModuleMemorySize);
+                    else
+                        n = sig[i.Id];
+
+                    float dfov = 0.78f;
+                    float dzoom = 20.0f;
+                    float dzooms = 20.0f;
+                    float dzoommin = 1.5f;
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        dfov = (float)numericUpDown1.Value;
+                        dzoom = dzooms = (float)numericUpDown2.Value;
+                        dzoommin = (float)numericUpDown3.Value;
+                    });
+
+                    bool isfov = false;
+                    bool iszoom = false;
+                    bool iszoomcur = false;
+                    bool iszoommin = false;
+
+                    int[] vx = new int[] { (int)(n.ToInt64() - i.MainModule.BaseAddress.ToInt64()) };
+
+                    switch (isFov)
+                    {
+                        case 0:
+                            isfov = iszoom = iszoomcur = true;
+                            break;
+                        case 1:
+                            isfov = true;
+                            break;
+                        case 2:
+                            iszoom = true;
+                            break;
+                        case 3:
+                            iszoommin = true;
+                            break;
+                        case 4:
+                            iszoomcur = true;
+                            break;
+                    }
+
+                    if(iszoom)
+                    {
+                        if (isFov == 0)
+                            dzoom = 20.0f;
+
+                        var addr = GetAddress(8, i, this.m_hProcess, vx, zoommax);
+                        Write(dzoom, this.m_hProcess, addr);
+                    }
+
+                    if (isFov == 0)
+                    {
+                        var addr = GetAddress(8, i, this.m_hProcess, vx, zoommax);
+                        Write(dzooms, this.m_hProcess, addr);
+                    }
+
+                    if (iszoomcur)
+                    {
+                        var addr = GetAddress(8, i, this.m_hProcess, vx, current);
+                        Write(dzoom, this.m_hProcess, addr);
+                    }
+
+                    if(iszoommin)
+                    {
+                        var addr = GetAddress(8, i, this.m_hProcess, vx, zoommin);
+                        Write(dzoommin, this.m_hProcess, addr);
+                    }
+
+                    if (isfov)
+                    {
+                        var addr = GetAddress(8, i, this.m_hProcess, vx, fofvcur);
+                        Write(dfov, this.m_hProcess, addr);
+                        addr = GetAddress(8, i, this.m_hProcess, vx, fofvmax);
+                        Write(dfov, this.m_hProcess, addr);
+                    }
+                }
+            }
+        }
+
+        private static void Write(float value, IntPtr handle, IntPtr address)
+        {
+            var buffer = BitConverter.GetBytes(value);
+            IntPtr written;
+            if (!(WriteProcessMemory(handle, address, buffer, buffer.Length, out written)))
+            {
+                
+            }
+        }
+
+        private static IntPtr GetAddress(byte size, Process process, IntPtr ptr, IEnumerable<int> offsets, int finalOffset)
+        {
+            var addr = process.MainModule.BaseAddress;
+            var buffer = new byte[size];
+            foreach (var offset in offsets)
+            {
+                IntPtr read;
+                if (!(ReadProcessMemory(ptr, IntPtr.Add(addr, offset), buffer, buffer.Length, out read)))
+                {
+                    throw new Exception("Unable to read process memory");
+                }
+                addr = (size == 8)
+                    ? new IntPtr(BitConverter.ToInt64(buffer, 0))
+                    : new IntPtr(BitConverter.ToInt32(buffer, 0));
+            }
+            return IntPtr.Add(addr, finalOffset);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+            ProcessAccessFlags dwDesiredAccess,
+            [MarshalAs(UnmanagedType.Bool)]
+                bool bInheritHandle,
+            int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            IntPtr nSize,
+            [Out]
+                out IntPtr lpNumberOfBytesRead);
+
+        public static IntPtr Scan(IntPtr hProcess, string pattern, IntPtr baseAddr, long maxsize = 0)
+        {
+            var patArray = GetPatternArray(pattern);
+
+            var posPtr = baseAddr;
+            var maxPtr = new IntPtr(baseAddr.ToInt64() + maxsize - patArray.Length);
+
+            var buff = new byte[4096];
+            IntPtr read;
+            int index;
+
+            while (posPtr.ToInt64() < maxPtr.ToInt64())
+            {
+                read = new IntPtr(Math.Min(maxPtr.ToInt64() - posPtr.ToInt64(), 4096));
+                if (ReadProcessMemory(hProcess, posPtr, buff, read, out read))
+                {
+                    index = FindArray(buff, patArray, 0, read.ToInt32());
+                    if (index != -1)
+                    {
+                        long ptr = posPtr.ToInt64() + index;
+                        ptr += patArray.Length;
+                        ptr += BitConverter.ToInt32(buff, index + patArray.Length - 4);
+                        return (IntPtr)ptr;
+                    }
+                    posPtr = new IntPtr(posPtr.ToInt64() + read.ToInt64() - patArray.Length + 1);
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private static byte?[] GetPatternArray(string pattern)
+        {
+            byte?[] arr = new byte?[pattern.Length / 2];
+
+            for (int i = 0; i < (pattern.Length / 2); i++)
+            {
+                string str = pattern.Substring(i * 2, 2);
+                if (str == "**")
+                    arr[i] = null;
+                else
+                    arr[i] = new byte?(Convert.ToByte(str, 0x10));
+            }
+
+            return arr;
+        }
+
+        private static int FindArray(byte[] buff, byte?[] pattern, int startIndex, int len)
+        {
+            len = Math.Min(buff.Length, len);
+
+            int i, j;
+            for (i = startIndex; i < (len - pattern.Length); i++)
+            {
+                for (j = 0; j < pattern.Length; j++)
+                    if (pattern[j].HasValue && buff[i + j] != pattern[j].Value)
+                        break;
+
+                if (j == pattern.Length)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static IntPtr ReadPointer(IntPtr handle, bool isX64, IntPtr address)
+        {
+            int size_t = isX64 ? 8 : 4;
+
+            byte[] lpBuffer = new byte[size_t];
+            IntPtr read;
+            if (!ReadProcessMemory(handle, address, lpBuffer, new IntPtr(size_t), out read) || read.ToInt64() != size_t)
+                return IntPtr.Zero;
+
+            if (isX64)
+                return new IntPtr(BitConverter.ToInt64(lpBuffer, 0));
+            else
+                return new IntPtr(BitConverter.ToInt32(lpBuffer, 0));
+        }
+
+        public static byte[] ReadBytes(IntPtr handle, IntPtr address, int length)
+        {
+            if (length <= 0 || address == IntPtr.Zero)
+                return null;
+
+            byte[] lpBuffer = new byte[length];
+
+            IntPtr read = IntPtr.Zero;
+
+            ReadProcessMemory(handle, address, lpBuffer, new IntPtr(length), out read);
+
+            return lpBuffer;
+        }
+
+        private void Default_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = (decimal)0.78;
+            numericUpDown2.Value = (decimal)20.0;
+            numericUpDown3.Value = (decimal)1.5;
+        }
+
+        private void RunSetZoom(int i)
+        {
+            try
+            {
+                new Thread(new ThreadStart(() =>
+                {
+                    ProcessLoad(i);
+                })).Start();
+            }
+            catch { }
+            saveValues();
+            Thread.Sleep(30);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            RunSetZoom(1);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            RunSetZoom(2);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            RunSetZoom(3);
+        }
+    }
+}
